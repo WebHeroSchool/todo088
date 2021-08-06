8cf4f092d17647ef0a346028f3986af5703c8116
Анастасия Бучацкая <nastena2001rus@mail.ru>
2021-08-06 16:59:18 +0300

    lesson 13

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
+import React from 'react';
 
-import logo from './logo.svg';
-import './App.css';
-import count from './number.js';
-import {length} from './number.js';
+const element = (<div>
+Добрый вечер,Ментор!)
+</div>);
 
-const name = 'Привет';
-const nuber = 26;
-const log = false;
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <p style= {{
-          color: 'blue'
-        }}>
-        {name}
-        , Ментор!
-        </p>
-
-        <p style= {{
-          color: 'yellow'
-        }}>
-        count *length = 
-        {count + length}
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export {element};
--- src/number.js
+++
@@ -1,5 +0,0 @@
-const count = 900;
-const n2 = 79;
-
-export default count;
-export {n2 as length};
