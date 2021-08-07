dba2c6dec112fc4ac9cdc32337e92e844a571de6
Анастасия Бучацкая <nastena2001rus@mail.ru>
2021-08-07 16:36:41 +0300

    lesson 21

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+import Footer from '../Footer/Footer';
+
+const todoItem = 'Написать новое приложение';
+
+const App = () => {
+  const items = [
+
+    {
+        value: 'Написать новое приложение'
+    },
+    {
+        value: 'прописать props'
+     },
+    {
+        value: 'сделать все дела'
+    }
+];
+return (
+<div>
+  <h1> Планы на день</h1>
+  <ItemList items={items}/>
+  <Footer count={4} />;
+</div>);
+}
+
+export default App;
