b44aaa3a4ac47ad3b6083136daded4bea226d6df
Анастасия Бучацкая <nastena2001rus@mail.ru>
2021-08-08 17:24:14 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ItemList from '../ItemList/ItemList';
+import Footer from '../Footer/Footer';
+import InputItem from '../InputItem/InputItem';
+
+const todoItem = 'Написать новое приложение';
 
 const App = () => (<div>
   <h1> Планы на день</h1>
