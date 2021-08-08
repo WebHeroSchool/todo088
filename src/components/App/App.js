aae446c2c07b4e088f40e647e0026f9df67b6907
Анастасия Бучацкая <nastena2001rus@mail.ru>
2021-08-08 09:57:51 +0300

    lesson 24
    выдает ошибку не пойму, в чем она заключается
    Error: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: object. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.
    
    Check the render method of `App`.

---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+import Footer from '../Footer/Footer';
+import styles from './App.module.css';
+
+const todoItem = 'Написать новое приложение';
+
+const App = () => {
+  const items = [
+
+    {
+        value: 'Написать новое приложение',
+        isDone: true
+    },
+    {
+        value: 'прописать props',
+        isDone: false
+     },
+    {
+        value: 'сделать все дела',
+        isDone: true
+    }
+];
+return (
+<div className={styles.wrap}>
+  <h1 className={styles.title}> Планы на день</h1>
+  <ItemList items={items}/>
+  <Footer count={4} />
+</div>);
+}
+
+export default App;
---
+++ src/components/Item/Item.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Item.module.css'
+import classnames from 'classnames';
+
+const Item = ({ value, isDone }) => (<span className={
+  classnames({
+    [styles.Item]: true,
+    [styles.done]: isDone
+  })
+}>
+  {value}
+</span>);
+
+export default Item;
