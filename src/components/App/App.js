92aae55bcd4131f4433cc1a96715119aae89fe50
Анастасия Бучацкая <nastena2001rus@mail.ru>
2021-08-09 10:10:28 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
+import InputItem from '../InputItem/InputItem';
 
 const todoItem = 'Написать новое приложение';
 
