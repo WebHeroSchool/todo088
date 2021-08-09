b505be7cc867a56585bf057e666ed20559789af6
Анастасия Бучацкая <nastena2001rus@mail.ru>
2021-08-09 11:26:32 +0300

    lesson 25

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField'
 
-const InputItem = () => (<div>ItputItem</div>);
+const InputItem = () => (<div>
+<TextField
+  id="standard-dense"
+  label="Добавить задание"
+  margin="dense"
+  />
+  </div>);
 
 export default InputItem;
