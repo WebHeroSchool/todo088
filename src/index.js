deb5a2c021cab4455d497a7fe23b1b07368ae96f
Анастасия Бучацкая <nastena2001rus@mail.ru>
2021-08-09 10:07:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import Footer from '../InputItem/InputItem';
 
 ReactDOM.render(<App />,document.getElementById('root'));
